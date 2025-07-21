11824cf205cbc20f42c8b0bc07b42dffb6845283
Vanessa0397 <vanessaagsangre99@gmail.com>
2025-07-21 11:55:34 +0800

    Create layout.js

---
+++ app/app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Vanessa Agsangre | Copywriter Portfolio',
+  description: 'Creative and compelling copywriting portfolio by Vanessa Agsangre.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
